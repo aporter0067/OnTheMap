64aae6ecef17eac5e7b55b85772d9f22a93422d5
Adam Porte <aporter0067@gmail.com>
2020-07-15 20:08:51 -0400

    Created Project
    On The Map is a project will show student locations posted by others.
    
    Purpose of this project is to gain experience in making network requests, accessing networked data using Apple's URL loading framework, and use core location and the MapKit framework to display annotated pins on a map.

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Adam Porter on 7/15/20.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
