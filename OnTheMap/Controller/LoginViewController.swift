7aa6b9a5f2c1ce3f2a27c77fcb94c7381249ce26
Adam Porte <aporter0067@gmail.com>
2020-07-20 20:50:29 -0400

    Added Outlets/Actions to loginViewController
    Added outlets to the username(email) and password textfields.
    
    Added outlet for udacity Logo
    
    Added IB actions for login button
    Added IB action for login via facebook button

--- OnTheMap/Controller/LoginViewController.swift
+++ OnTheMap/Controller/LoginViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var udacityLogo: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func loginViaFacebook(_ sender: Any) {
+    }
 }
 
