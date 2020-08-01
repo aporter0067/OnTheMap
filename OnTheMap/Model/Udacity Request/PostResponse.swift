39b8eff65482aef6724c5d27dd726776a415cfb4
Adam Porte <aporter0067@gmail.com>
2020-08-01 11:00:20 -0400

    Post Response and StudentInfo
    structs created to help parse JSON data for posting Student Locations

---
+++ OnTheMap/Model/Udacity Request/PostResponse.swift
@@ -0,0 +1,14 @@
+//
+//  PostResponse.swift
+//  OnTheMap
+//
+//  Created by Adam Porter on 7/30/20.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import Foundation
+
+struct PostResponse: Codable {
+    let createdAt: String
+    let updatedAt: String
+}
---
+++ OnTheMap/Model/Udacity Request/StudentInfo.swift
@@ -0,0 +1,19 @@
+//
+//  StudentInfo.swift
+//  OnTheMap
+//
+//  Created by Adam Porter on 7/30/20.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import Foundation
+
+struct StudentInfo {
+    var uniqueKey: String
+    var firstName: String
+    var lastName: String
+    var mapString: String
+    var mediaURL: String
+    var latitude: String
+    var longitude: String
+}
