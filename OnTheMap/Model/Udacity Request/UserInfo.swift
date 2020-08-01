8a762ab3946b1bae408604e7f845cf3d1b0f9ad6
Adam Porte <aporter0067@gmail.com>
2020-08-01 10:57:11 -0400

    Create User Info Struct
    created User Info Struct to assist in Parsing the JSON data

---
+++ OnTheMap/Model/Udacity Request/UserInfo.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfo.swift
+//  OnTheMap
+//
+//  Created by Adam Porter on 8/1/20.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import Foundation
+
+struct UserInfo: Codable {
+   
+    let key: String
+    let firstName: String
+    let lastName: String
+}
